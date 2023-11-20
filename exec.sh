971e88e0dadd7b25a8c6c3b33d6827f566fd9b51
MD IMRAN HOSSEN <127003890+hossen1907012@users.noreply.github.com>
2023-11-20 05:36:07 +0000

    Init

--- exec.sh
+++ exec.sh
@@ -1,3 +1,14 @@
+rm 1907012.tab.c
+rm 1907012.tab.h
+rm lex.yy.c
+
 bison -d 1907012.y
 flex 1907012.l
 sudo gcc lex.yy.c 1907012.tab.c -o c.out
+./c.out
+rm c.out
+
+
+rm 1907012.tab.c
+rm 1907012.tab.h
+rm lex.yy.c
